7ea75f22f3d7348343223a2a37b70f57910a6b6e
BuildTools <totogonzalezfernandez@gmail.com>
2018-03-05 20:07:23 -0500

    go

---
+++ main.js
@@ -0,0 +1,14 @@
+var searchParams = new URLSearchParams(window.location.search);
+
+var page = searchParams.get('p');
+
+var dir = document.location.protocol + "/" + document.location.pathname.split("/").slice(0, -1).join("/") + "/pages/" + page + ".html"
+
+
+console.log(dir);
+
+
+$.get("./pages/" + page + ".html", function(data) {
+    $("div#content").html(data);
+    alert("Load was performed.");
+});
